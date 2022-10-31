532f54d3f84c2e5034dd25217339c617159a8bbe
Lorenzo Ruozzi <lruozzi@webgriffe.com>
2022-10-31 18:52:27 +0100

    Align to plugin skeleton v1.12

--- src/LRuozzi9SyliusElasticsearchPlugin.php
+++ src/LRuozzi9SyliusElasticsearchPlugin.php
@@ -6,8 +6,14 @@ namespace LRuozzi9\SyliusElasticsearchPlugin;
 
 use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use function dirname;
 
 final class LRuozzi9SyliusElasticsearchPlugin extends Bundle
 {
     use SyliusPluginTrait;
+
+    public function getPath(): string
+    {
+        return dirname(__DIR__);
+    }
 }
