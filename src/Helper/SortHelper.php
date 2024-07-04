b79b514267bd72babf2791f52a71bf93328b5351
Luca Gallinari <luke.gallinari@gmail.com>
2024-07-04 14:31:30 +0200

    Pass taxon to sort helper method

--- src/Helper/SortHelper.php
+++ src/Helper/SortHelper.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Webgriffe\SyliusElasticsearchPlugin\Helper;
 
+use Sylius\Component\Core\Model\TaxonInterface;
+
 final class SortHelper implements SortHelperInterface
 {
     public function retrieveSearchSorting(array $sortingQueryParams = []): array
@@ -11,7 +13,7 @@ final class SortHelper implements SortHelperInterface
         return $sortingQueryParams; // If empty it will sort by _score desc as default
     }
 
-    public function retrieveTaxonSorting(array $sortingQueryParams = []): array
+    public function retrieveTaxonSorting(array $sortingQueryParams = [], ?TaxonInterface $taxon = null): array
     {
         if ($sortingQueryParams === []) {
             return ['position' => 'asc'];
