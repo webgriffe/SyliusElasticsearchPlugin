b38297411bf50ed6aa6e82204718a3b16cbe5e18
Lorenzo Ruozzi <lruozzi@webgriffe.com>
2026-01-23 11:12:15 +0100

    Add routes name in page

--- config/packages/sylius_twig_hooks.php
+++ config/packages/sylius_twig_hooks.php
@@ -38,6 +38,13 @@ return static function (ContainerConfigurator $container): void {
     // Shop
     $container->extension('sylius_twig_hooks', [
         'hooks' => [
+            'sylius_shop.base#javascripts' => [
+                'routes' => [
+                    'template' => '@WebgriffeSyliusElasticsearchPlugin/shop/shared/layout/base/routes.html.twig',
+                    'priority' => 100,
+                ],
+            ],
+
             'webgriffe_sylius_elasticsearch.product.index' => [
                 'content' => [
                     'template' => '@SyliusShop/product/index/content.html.twig',
